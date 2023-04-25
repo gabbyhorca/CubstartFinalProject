81fcbaebbfefdbf80eaff748e77ddb6e80a1901a
jmooney <jasonmooney@berkeley.edu>
2023-04-24 18:04:37 -0700

    Add arrow asset and addtoschedule boilerplate

--- DartApp/DartApp/Pages/AddToSchedule.swift
+++ DartApp/DartApp/Pages/AddToSchedule.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct AddToSchedule: View{
+    var body: some View {
+       VStack {
+          Text("Hi")
+       }
+    }
+}
+
+struct AddToSchedule_Previews: PreviewProvider {
+    static var previews: some View {
+        AddToSchedule()
+    }
+}
