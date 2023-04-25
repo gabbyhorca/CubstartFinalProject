4bdc48382430b1dad4489a113297d58a483cd931
jmooney <jasonmooney@berkeley.edu>
2023-04-24 21:31:41 -0700

    Finish Schedule

--- DartApp/DartApp/Classes/Trip.swift
+++ DartApp/DartApp/Classes/Trip.swift
@@ -13,11 +13,15 @@ public class Trip : ObservableObject, Identifiable {
     var time : String
     var date : String
     var trainLine: String
+    var tripStart: String
+    var tripDestination: String
     
-    init(name: String, time: String, date: String, trainLine: String) {
+    init(name: String, time: String, date: String, trainLine: String, tripStart: String, tripDestination: String) {
         self.name = name
         self.time = time
         self.date = date
         self.trainLine = trainLine
+        self.tripStart = tripStart
+        self.tripDestination = tripDestination
     }
 }
--- DartApp/DartApp/Pages/Schedule.swift
+++ DartApp/DartApp/Pages/Schedule.swift
@@ -12,25 +12,80 @@ import SwiftUI
 struct Schedule: View {
     @State var tripList: [Trip] = []
     
-    @StateObject var t1 : Trip = Trip(name: "test", time: "3:00", date: "3.11.23", trainLine: "red")
+    @StateObject var dummyt1 : Trip = Trip(name: "Test Trip 1", time: "3:00", date: "5/11/23", trainLine: "Red", tripStart: "Downtown Berkeley", tripDestination: "Embarcadero")
+    @StateObject var dummyt2 : Trip = Trip(name: "Test Trip 2", time: "5:00", date: "5/12/23", trainLine: "Blue", tripStart: "Downtown Berkeley", tripDestination: "Ashby")
     
-    func addTripList () {
-        tripList = [t1]
+    func addTripListItems() {
+        tripList = [dummyt1, dummyt2]
+        //Add your objects to your list here!
+        
     }
     
     var body: some View {
         VStack {
-            Button("Back") {
-                Image("backArrow")
-            }.position(x: -90, y: 300)
-            Text("Schedule")
-            
-            //ForEach(tripList) {i in
-            //  VStack(alignment: .leading)
-            //}
-            //Text
-            //}
-        }
+            HStack{
+                Button(action: {
+                }, label: {
+                    HStack {
+                        Image("backArrow")
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(50)
+                            .frame(width: 20, height: 20)
+                        Text("Back").foregroundColor(.black)
+                    }
+                })
+                Spacer()
+            }
+            VStack{
+                HStack {
+                    
+                    Text("Current Schedule")
+                        .padding(.top, 20)
+                        .font(.system(size: 24, weight: .light, design: .default))
+                    Spacer()
+                }
+                Color
+                    .black
+                    .frame(width: 350, height: 5 / UIScreen.main.scale)
+                    .multilineTextAlignment(.leading)
+                    .padding(-10)
+                
+                ScrollView {
+                        VStack(alignment: .leading) {
+                            ForEach(tripList) {i in
+                                VStack{
+                                    HStack{
+                                        Text(i.date)
+                                            .font(.system(size: 15, weight: .light, design: .default))
+                                        Text(i.name)
+                                        Spacer()
+                                        Button("Edit"){
+                                            
+                                        }.frame(maxWidth: .infinity, alignment: .trailing)
+                                        Spacer()
+                                    }
+                                    HStack {
+                                        
+                                        Text(i.time)
+                                        Text(i.tripStart)
+                                        Text(" - ")
+                                        Text(i.tripDestination)
+                                        Spacer()
+                                    }
+                                    Color
+                                                   .gray
+                                                   .frame(width: 350, height: 1 / UIScreen.main.scale)
+                                                   .multilineTextAlignment(.leading)
+                                    Spacer()
+                                }
+                            }
+                        }
+                    }
+                Spacer()
+            }
+        }.onAppear(perform: addTripListItems)
+            .padding()
     }
 }
 
