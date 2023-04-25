3cf85f1565fe15f37098411f2d6d993331ec72a3
raven <ravennguyen02@gmail.com>
2023-04-24 19:48:34 -0700

    SignIn screen

--- DartApp/DartApp/Pages/SignIn.swift
+++ DartApp/DartApp/Pages/SignIn.swift
@@ -9,9 +9,77 @@ import Foundation
 import SwiftUI
 
 struct SignIn: View{
+    @State var email = ""
+    @State var password = ""
+    @State var loggedIn = false
+    @State var forgotPassword = false
+    @State var signUp = false
     var body: some View {
        VStack {
-          Text("Hi")
+           Image("bart lines")
+               .resizable()
+               .scaledToFit()
+               .frame(width: 400, height: 400, alignment: .top)
+               .aspectRatio(1.0, contentMode: .fit)
+           
+           Text("Username")
+               .multilineTextAlignment(.leading)
+               .font(.body)
+               .frame(maxWidth: 300, alignment: .leading)
+           
+           TextField("Username", text: $email)
+               .multilineTextAlignment(.leading)
+               .textFieldStyle(.roundedBorder)
+               .textFieldStyle(PlainTextFieldStyle())
+               .padding([.horizontal], 4)
+               .frame(maxWidth: 300, alignment: .leading)
+               .disableAutocorrection(true)
+               .autocapitalization(.none)
+               .frame(height: 40)
+           
+
+           Text("Password")
+               .multilineTextAlignment(.leading)
+               .font(.body)
+               .frame(maxWidth: 300, alignment: .leading)
+           
+           HStack {
+               SecureField("Password", text: $password)
+                   .multilineTextAlignment(.leading)
+                   .textFieldStyle(.roundedBorder)
+                   .textFieldStyle(PlainTextFieldStyle())
+                   .padding([.horizontal], 4)
+                   .frame(maxWidth: 300, alignment: .leading)
+                   .disableAutocorrection(true)
+                   .autocapitalization(.none)
+//               Image(systemName: "arrow.left.to.line")
+               
+           }
+           Button("Forgot Password?") {
+               forgotPassword.toggle()
+           }
+           .toggleStyle(.automatic)
+           .foregroundColor(.blue)
+           .frame(maxWidth: 300, alignment: .trailing)
+           .font(.system(size: 13))
+           
+           HStack {
+               Button("Log In") {
+                   loggedIn.toggle()
+               }
+               .frame(height: 100)
+               .buttonStyle(.bordered).tint(.blue)
+               .toggleStyle(.button)
+               Image(systemName: "arrow.right")
+           }
+           
+           
+           HStack {
+               Text("Don't have an account?")
+               Button("Sign Up!") {
+                   signUp.toggle()
+               }
+           }
        }
     }
 }
