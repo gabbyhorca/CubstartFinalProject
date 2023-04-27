1acc1f61dccc0e941526055d6fae33569d9480ce
jmooney <jasonmooney@berkeley.edu>
2023-04-26 21:06:32 -0700

    more api stuff

--- DartApp/DartApp/Api/Api.swift
+++ DartApp/DartApp/Api/Api.swift
@@ -7,7 +7,35 @@
 
 import Foundation
 
-func fetch realTimeInfo() async-> RealTimeInfo? {
-   let  url = URL(
-      
+func fetchRealTimeInfo() async-> RealTimeInfo? {
+    guard let url = URL(string: "https://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=QJJR-5LTA-978T-DWEI&json=y")
+    else {
+        return nil
+    }
+    
+    let api_key = "QJJR-5LTA-978T-DWEI"
+    
+    do {
+        let (data, _) = try await URLSession.shared.data(from: url)
+        if let decodedResponse = try? JSONDecoder().decode(RealTimeInfo.self, from: data) {
+                    return decodedResponse
+                }
+    } catch {
+        return nil
+    }
+    return nil
+}
+
+func fetchRealTimeInfoContinuous() async -> RealTimeInfo {
+    // Try to get a Dog
+    var newRealTimeInfo = (await fetchRealTimeInfo()) ?? nil
+    
+    // Check if there is a Dog
+    while (newRealTimeInfo == nil) {
+        // Try again to get Dog
+        newRealTimeInfo = (await fetchRealTimeInfo()) ?? nil
+    }
+    
+    // Can force-unwrap because newDog is guaranteed a dog.
+    return newRealTimeInfo!
 }
--- DartApp/DartApp/Api/RealTimeInfo.swift
+++ DartApp/DartApp/Api/RealTimeInfo.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct RealTimeInfo: Codable {
+    let
+}
